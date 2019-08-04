c8afa875ec9725b8749beee42b193bf0e28e1f92
Khalid Umar <khalid.umar101@gmail.com>
2019-08-04 16:05:32 -0400

    fixed ftore import

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/App'
 import './styles.scss';
 import configureStore from './redux/configureStore'
+import { addCurrency } from './redux/actions/selectCurrencies';
 
 const store  = configureStore()
-console.log(store.getState())
 
+
+store.dispatch(addCurrency('1'))
+
+console.log(store.getState())
 ReactDOM.render(
     <App />
     , document.getElementById('root')
